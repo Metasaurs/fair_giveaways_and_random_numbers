416e58c2fff9669e7dd0749d9643df8d6d3ef272
Dan Ξ <danandreist@gmail.com>
2021-10-16 20:10:30 +0300

    Update calc_hashes_original.sh

--- calc_hashes_original.sh
+++ calc_hashes_original.sh
@@ -1,3 +1,3 @@
-cd /home/me/workspace_backend/hash_and_randomizer/original_images
+cd /PATH_TO_FOLDER_WITH_ORIGINAL_IMAGES
 find . -type f -exec sha256sum {} \; > ../sha256_of_images.txt
 echo "=========== hashes written to ../sha256_of_images.txt =========== "
