cdc876e560f377dd7987930f9f4000cd3beda1c3
Dan Ξ <danandreist@gmail.com>
2021-10-16 20:11:05 +0300

    Add files via upload

---
+++ shuffling.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
+
+const chainlinkRandomNumber = "9223372036854775807922337203685477580792233720368547758079223372036854775807"
+
+//max int64  -> random number from chainlink will be cut to 18 digits
+//9223372036854775807 -> this is max int64 in go, as max value for 'seed'
+func main() {
+	fmt.Printf("running script at @ unix nano %d\n", time.Now().UnixNano())
+
+	//chainlinkRandomNumber is too big, get the first 18 digits
+	seedStr := chainlinkRandomNumber[0:18]
+	seed, err := strconv.ParseInt(seedStr, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	rand.Seed(seed)
+
+	nftIDs := make([]int, 10000)
+	for i := 1; i <= 9999; i++ {
+		nftIDs[i] = i
+	}
+
+	rand.Shuffle(len(nftIDs), func(i, j int) {
+		nftIDs[i], nftIDs[j] = nftIDs[j], nftIDs[i]
+	})
+
+	printToFile("shuffled.txt", nftIDs)
+	fmt.Println(nftIDs)
+}
+
+func printToFile(filePath string, values []int) {
+	f, err := os.Create(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	for _, value := range values {
+		fmt.Fprintln(f, value) // print values to f, one per line
+	}
+}
